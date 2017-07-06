4295ed57447f7002b2c617c1675e3c9e22089826
Zeti123 <globart1234567890@gmail.com>
2017-07-06 19:14:13 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,44 @@
 #include <SFML/Graphics.hpp>
-#include <bits/stdc++.h>
+#include "Events.cpp"
+
 using namespace sf;
 using namespace std;
-int main(){
-	menu();
-	
-	
-	
-	
-	
-	
-	
+
+const int szerokosc=1000,wysokosc=700,METR=50;
+double FPS;
+
+RenderWindow window(VideoMode(szerokosc, wysokosc), "Gra");
+// OnStart,OnClose,OnKey,OnFps
+int main()
+{
+	Time time;
+	Clock clock;
+	Event event;
+	Mouse mouse;
+	OnStart();
+	while (window.isOpen())
+	{
+		window.clear();
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed) OnClose(), window.close();
+			if (event.type == Event::Event::KeyPressed) OnKey(&event);
+			if (event.type == Event::Event::MouseButtonPressed)
+			{
+				Vector2i a=mouse.getPosition(window);
+				OnClick(a.x, a.y, mouse.isButtonPressed(Mouse::Left), mouse.isButtonPressed(Mouse::Right));
+			}
+		}
+		OnFps(FPS);
+		//***Dodatkowe funkcje wykonywane co klatkę***
+		{
+			
+			
+		}
+		//	
+		window.display();
+		time=clock.getElapsedTime();
+   		FPS=time.asSeconds();
+		clock.restart(); 
+	}
 }
